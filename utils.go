c6816b67a1b02dce951c795d34107c1e50bd3f0d
Bruno Voisin <bruno@voisin.dev>
2024-10-06 15:23:36 +0200

    refactor: remove unused function and inline another one

--- utils.go
+++ utils.go
@@ -5,13 +5,5 @@ import "math"
 func Distance(p1, p2 Planet) float64 {
 	dx := p1.Position.X - p2.Position.X
 	dy := p1.Position.Y - p2.Position.Y
-	return math.Ceil(math.Sqrt(Pow2(dx) + Pow2(dy)))
-}
-
-func Pow2(x float64) float64 {
-	return x * x
-}
-
-func Pow2Int(x int) int {
-	return x * x
+	return math.Ceil(math.Sqrt(dx*dx + dy*dy))
 }
