db67e44f9014b9e84b87365740f853de969e9f0c
Bruno Voisin <bruno@voisin.dev>
2024-10-11 18:12:12 +0200

    feat(brunobot): consider growth when sending fleet

--- players/brunobot/player.go
+++ players/brunobot/player.go
@@ -11,16 +11,16 @@ func NewBrunoBot() planetwars.Player {
 }
 
 // DoTurn implements Player.
-func (b *brunoBot) DoTurn(planetMap planetwars.Map) []planetwars.Order {
-	candidates := computeCandidates(planetMap)
-	remainingShips := make(map[planetwars.PlanetID]planetwars.Ships, len(planetMap.Planets))
+func (b *brunoBot) DoTurn(pwMap planetwars.Map) []planetwars.Order {
+	candidates := computeCandidates(pwMap)
+	remainingShips := make(map[planetwars.PlanetID]planetwars.Ships, len(pwMap.Planets))
 	orders := make([]planetwars.Order, 0)
 	for _, candidate := range candidates {
 		_, ok := remainingShips[candidate.Source]
 		if !ok {
-			remainingShips[candidate.Source] = planetMap.PlanetByID(candidate.Source).Ships
+			remainingShips[candidate.Source] = pwMap.PlanetByID(candidate.Source).Ships
 		}
-		fleetShips := planetMap.PlanetByID(candidate.Dest).Ships + 1
+		fleetShips := computeNeededFleetShips(pwMap, candidate.Source, candidate.Dest)
 		remainingShips[candidate.Source] -= fleetShips
 		if remainingShips[candidate.Source] >= 10 {
 			orders = append(
@@ -30,4 +30,18 @@ func (b *brunoBot) DoTurn(planetMap planetwars.Map) []planetwars.Order {
 		}
 	}
 	return orders
+}
+
+func computeNeededFleetShips(
+	pwMap planetwars.Map,
+	source planetwars.PlanetID,
+	dest planetwars.PlanetID,
+) planetwars.Ships {
+	pSrc := pwMap.PlanetByID(source)
+	pDest := pwMap.PlanetByID(dest)
+	var growthAtArrival int
+	if pDest.Owner == planetwars.Opponent {
+		growthAtArrival = int(planetwars.Distance(pSrc, pDest) * float64(pDest.Growth))
+	}
+	return pDest.Ships + planetwars.Ships(growthAtArrival) + 1
 }
